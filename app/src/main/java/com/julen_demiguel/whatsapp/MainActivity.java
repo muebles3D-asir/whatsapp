03c997e8d61594f1736cf2f8be0b6707bad45e03
joroz10 <javieroroz03@gmail.com>
2023-01-29 14:55:32 +0100

    Toolbar menu agregado

--- app/src/main/java/com/julen_demiguel/whatsapp/MainActivity.java
+++ app/src/main/java/com/julen_demiguel/whatsapp/MainActivity.java
@@ -3,7 +3,12 @@ package com.julen_demiguel.whatsapp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
@@ -12,12 +17,12 @@ public class MainActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     androidx.appcompat.widget.Toolbar toolbar;
+    TabLayout.Tab tabComunidad,tabChats,tabEstado,tabLlamadas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Prueba de cambio (Álvaro)
-
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.addTab(tabLayout.newTab().setText("CHATS"));
         tabLayout.addTab(tabLayout.newTab().setText("ESTADOS"));
@@ -26,7 +31,27 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Whatsapp");
         setSupportActionBar(toolbar);
+        toolbar.setSubtitleTextColor(Color.WHITE);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_nuevoGrupo:
+                return true;
+            case R.id.action_nuevaDifusion:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
