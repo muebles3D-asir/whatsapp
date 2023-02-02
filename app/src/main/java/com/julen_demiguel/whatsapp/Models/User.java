a3638166fdca97d856acad4b0e28c844b2046aab
julen <julendemi02@gmail.com>
2023-02-02 09:27:20 +0100

    Clase user

--- app/src/main/java/com/julen_demiguel/whatsapp/Models/User.java
+++ app/src/main/java/com/julen_demiguel/whatsapp/Models/User.java
@@ -1,4 +1,50 @@
-package com.julen_demiguel.whatsapp.Clases;
+package com.julen_demiguel.whatsapp.Models;
+
+import java.util.Date;
 
 public class User {
+    String name;
+    String corre;
+    //String img;
+    String password;
+    Date fechaNac;
+
+    public User(String name, String corre, String password, Date fechaNac) {
+        this.name = name;
+        this.corre = corre;
+        this.password = password;
+        this.fechaNac = fechaNac;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCorre() {
+        return corre;
+    }
+
+    public void setCorre(String corre) {
+        this.corre = corre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
 }
