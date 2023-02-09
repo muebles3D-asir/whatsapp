7725ba18d38f7e8d508e5236bfdcb87d96fb90b9
julen <julendemi02@gmail.com>
2023-02-09 09:26:54 +0100

    Refactorizacion

--- app/src/main/java/com/julen_demiguel/whatsapp/Models/ChatGroup.java
+++ app/src/main/java/com/julen_demiguel/whatsapp/Models/ChatGroup.java
@@ -1,12 +1,6 @@
 package com.julen_demiguel.whatsapp.Models;
-
-<<<<<<< HEAD
 import com.julen_demiguel.whatsapp.Application.MyApplication;
-
-=======
->>>>>>> c0ad1c1fd880d7d4bef35b07293fc1e51941e2fd
 import java.util.ArrayList;
-
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
@@ -32,12 +26,11 @@ public class ChatGroup extends RealmObject {
         this.messages = messages;
     }
 
-    public String addUsr(User newUser){
-        if(participants.contains(newUser)){
-            return "Ese usuario ya esta en el grupo";
+    public Boolean addUsr(User newUser){
+        if(participants.equals(newUser)){
+            return false;
         } else{
-            participants.add(newUser);
-            return "usuario añadido";
+            return true;
         }
 
 
