3865e7fdd6a9a17ea6909f952f8cf29e5c0a6f9d
alvaroroco <alvaroroco@gmail.com>
2023-02-25 21:43:48 +0100

    Arreglo fallo chat

--- app/src/main/java/com/julen_demiguel/whatsapp/activities/ChatActivity.java
+++ app/src/main/java/com/julen_demiguel/whatsapp/activities/ChatActivity.java
@@ -6,43 +6,35 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.julen_demiguel.whatsapp.Application.MyApplication;
 import com.julen_demiguel.whatsapp.Models.Chat;
 import com.julen_demiguel.whatsapp.Models.Message;
 import com.julen_demiguel.whatsapp.R;
 import com.julen_demiguel.whatsapp.adapters.MessageRecyclerDataAdapter;
-import com.julen_demiguel.whatsapp.fragments.ChatsFragment;
 
 import java.util.Date;
-import java.util.List;
 
 import io.realm.Realm;
-import io.realm.RealmConfiguration;
 import io.realm.RealmList;
-import io.realm.RealmResults;
 
 public class ChatActivity extends AppCompatActivity {
 
     MessageRecyclerDataAdapter messageDataAdapter;
     RecyclerView rvChat;
+
     androidx.appcompat.widget.Toolbar toolbar;
     RealmList<Message> messages;
-    EditText etMessage;
-    Button btnSend;
     Realm realm;
     Chat chat;
     int id;
-    EditText mensaje;
     Date date = new Date();
 
+    TextView tvNombre;
+    EditText etMensaje;
     Button botonSend;
 
 
@@ -51,10 +43,6 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         realm = Realm.getDefaultInstance();
-        RealmConfiguration config = new RealmConfiguration.Builder()
-                .allowWritesOnUiThread(true)
-                .build();
-
         try{
             Bundle bundle = getIntent().getExtras();
             id = bundle.getInt("id");
@@ -62,46 +50,40 @@ public class ChatActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(chat.getOtherUser().getName());
         } catch (NullPointerException e ){}
 
-        mensaje = findViewById(R.id.etMessage);
+        etMensaje = findViewById(R.id.etMessage);
         botonSend = findViewById(R.id.btnSend);
         toolbar = findViewById(R.id.chatToolbar);
+        rvChat = findViewById(R.id.rvChat);
+        tvNombre = findViewById(R.id.nameShowInChat);
+        tvNombre.setText(chat.getOtherUser().getName());
+
         toolbar.setTitle(chat.getOtherUser().getName());
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent toMain = new Intent(ChatActivity.this,MainActivity.class);
-                startActivity(toMain);
-            }
+        toolbar.setNavigationOnClickListener(v -> {
+            Intent toMain = new Intent(ChatActivity.this,MainActivity.class);
+            startActivity(toMain);
         });
 
-        botonSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mensaje.getText().length() > 0){
-                    Message newMessage = new Message(mensaje.getText().toString(), MyApplication.currentUser, date);
-                    realm.executeTransactionAsync(new Realm.Transaction() {
-                        @Override
-                        public void execute(Realm realm) {
-                            Message newMessage = new Message(mensaje.getText().toString(), MyApplication.currentUser, date);
-                            chat.getMessages().add(newMessage);
-                        }
-                    });
-                }
-            }
-        });
-
-        rvChat = findViewById(R.id.rvChat);
         messages = chat.getMessages();
-
-
         messageDataAdapter = new MessageRecyclerDataAdapter(messages, chat.getOtherUser());
         rvChat.setAdapter(messageDataAdapter);
         rvChat.setLayoutManager(new GridLayoutManager(this, 1));
+
+        botonSend.setOnClickListener(v -> {
+            if(etMensaje.getText().length() > 0){
+                Message newMessage = new Message(etMensaje.getText().toString(), MyApplication.currentUser, date);
+                realm.beginTransaction();
+                chat.getMessages().add(newMessage);
+                realm.commitTransaction();
+                etMensaje.setText("");
+            }
+        });
+        messages.addChangeListener(boards -> messageDataAdapter.notifyDataSetChanged());
     }
+
     @Override
     public void onBackPressed() {
         Intent toMain = new Intent(ChatActivity.this,MainActivity.class);
